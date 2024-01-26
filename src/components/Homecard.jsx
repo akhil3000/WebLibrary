d4f715e48fc5bcdebff964a4947d9ccf8082c71d
AkhilPolisetty <akhilpolisetty456@gmail.com>
2024-01-26 18:55:19 +0530

    new commit

---
+++ src/components/Homecard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './button.css'
+import { Link } from 'react-router-dom'
+const Homecard = ({path,title}) => {
+  return (
+    <div>
+    <Link to={path}>
+     <button className="a1">{title}
+     <button className="a2">Bookmark Course</button>
+     </button>
+      </Link>
+    </div>
+  )
+}
+
+export default Homecard
--- src/components/WebDevelopment/WebDevlopment.jsx
+++ src/components/WebDevelopment/WebDevlopment.jsx
@@ -1,67 +1,35 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import './WebDevelopment.css'
-
+import Homecard from '../Homecard';
 
 
 const WebDevlopment = () => {
   return (
     <div className="Hello">
-       <Link to="/WebPrerequitises">
-     <button className="a1">Web Development Prerequitises</button>
-      </Link>
-      <Link to="/WebDevelopmentBasics">
-     <button className="a1">Web Development Basics</button>
-      </Link>
-      <Link to="/GitAndGithub">
-       <button className="a1">Git And Github</button>
-      </Link> 
-      <Link to="/HtmlAndCss">
-      <button className="a1">HTML And CSS</button>
-      </Link>
-      <Link to="/JavaScript">
-      <button className="a1">JavaScript</button>
-      </Link>
-      <Link to="/Reactjs">
-      <button className="a1">ReactJs</button>
-      </Link>
-       <Link to="/BackendBasics">
-       <button className="a1">Backend Basics</button>
-       </Link>
-       <Link to="/MongoDB">
-       <button className="a1">MongoDB</button>
-       </Link>
-       <Link to="/NodeANDExpress">
-       <button className="a1">Node And Express Js</button>
-       </Link>
-       <Link to="/MERNStackProjects">
-       <button className="a1">MERN Stack Projects</button>
-       </Link>
-       <Link to="/NextJs">
-       <button className="a1">NextJs</button>
-       </Link>
-       <Link to="/MicroServicesAndCloud">
-       <button className="a1">MicroServicesAndCloud</button>
-       </Link>
-       <div className="hii">
+      <Homecard path="/WebPrerequitises" title="Web Development Prerequitises"/>
+      <Homecard path="/WebDevelopmentBasics" title="Web Development Basics"/>
+      <Homecard path="/GitAndGithub" title="Git And Github"/>
+      <Homecard path="/HtmlAndCss" title="HTML And CSS"/>
+      <Homecard path="/JavaScript" title="JavaScript"/>
+      <Homecard path="/Reactjs" title="ReactJs"/>
+      <Homecard path="/BackendBasics" title="Backend Basics"/>
+      <Homecard path="/MongoDB" title="MongoDB"/>
+      <Homecard path="/NodeANDExpress" title="Node And Express Js"/>
+      <Homecard path="/MERNStackProjects" title="MERN Stack Projects"/>
+      <Homecard path="/NextJs" title="NextJs"/>
+      <Homecard path="/MicroServicesAndCloud" title="MicroServicesAndCloud"/>
+      <div className="hii">
        One Tech Stack is completed you can learn further to improve your Knowledge
        </div>
-       <Link to="/Spring">
-       <button className="a1">Spring</button>
-       </Link>
-       <Link to="/SystemDesign">
-       <button className="a1">SystemDesign</button>
-       </Link>
-       <Link to="/BlockChainAndWeb3">
-       <button className="a1">BlockChainAndWeb3</button>
-       </Link>
-       <Link to="/Deployments">
-       <button className="a1">Deployments</button>
-       </Link>
-       <Link to="/WebDevCommunity">
-       <button className="a1">Web Development Community</button>
-       </Link>
-    </div>
+      <Homecard path="/Spring" title="Spring"/>
+      <Homecard path="/SystemDesign" title="System Design"/>
+      <Homecard path="/BlockChainAndWeb3" title="BlockChain And Web3"/>
+      <Homecard path="/Deployments" title="Deployments"/>
+      <Homecard path="/WebDevCommunity" title="Web Development Community"/>
+      
+      
+       </div>
   )
 }
 
