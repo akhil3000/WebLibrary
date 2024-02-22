834642dbcc3983fac5c40e45d97a7d98b6198ef5
viplav1709 <148878801+viplav1709@users.noreply.github.com>
2024-02-22 23:06:50 +0530

    Code changed and added util for static data

--- src/components/WebDevelopment/WebDevlopment.jsx
+++ src/components/WebDevelopment/WebDevlopment.jsx
@@ -1,47 +1,44 @@
-import React from 'react'
-import {Link} from "react-router-dom";
-import './WebDevelopment.css'
-import Homecard from '../Homecard';
-
+import React from "react";
+import "./WebDevelopment.css";
+import Homecard from "../Homecard";
 
 const WebDevlopment = () => {
   return (
     <div className="Hello">
-       <div className="hii">
-        Basics And Frontend
-       </div>
-       <div className="blocks">
-      <Homecard path="/WebPrerequitises" title="Web Development Prerequitises"/>
-      <Homecard path="/WebDevelopmentBasics" title="Web Development Basics"/>
-      <Homecard path="/GitAndGithub" title="Git And Github"/>
-      <Homecard path="/HtmlAndCss" title="HTML And CSS"/>
-      <Homecard path="/JavaScript" title="JavaScript"/>
-      <Homecard path="/Reactjs" title="ReactJs"/>
+      <div className="hii">Basics And Frontend</div>
+      <div className="blocks">
+        <Homecard
+          path="/WebPrerequitises"
+          title="Web Development Prerequitises"
+        />
+        <Homecard path="/WebDevelopmentBasics" title="Web Development Basics" />
+        <Homecard path="/GitAndGithub" title="Git And Github" />
+        <Homecard path="/HtmlAndCss" title="HTML And CSS" />
+        <Homecard path="/JavaScript" title="JavaScript" />
+        <Homecard path="/Reactjs" title="ReactJs" />
       </div>
-      <div className="hii">
-       Backend & Advanced
-       </div>
-       <div className="blocks">
-      <Homecard path="/BackendBasics" title="Backend Basics"/>
-      <Homecard path="/MongoDB" title="MongoDB"/>
-      <Homecard path="/NodeANDExpress" title="Node And Express Js"/>
-      <Homecard path="/MERNStackProjects" title="MERN Stack Projects"/>
-      <Homecard path="/NextJs" title="NextJs"/>
-      <Homecard path="/MicroServicesAndCloud" title="MicroServices And Cloud"/>
+      <div className="hii">Backend & Advanced</div>
+      <div className="blocks">
+        <Homecard path="/BackendBasics" title="Backend Basics" />
+        <Homecard path="/MongoDB" title="MongoDB" />
+        <Homecard path="/NodeANDExpress" title="Node And Express Js" />
+        <Homecard path="/MERNStackProjects" title="MERN Stack Projects" />
+        <Homecard path="/NextJs" title="NextJs" />
+        <Homecard
+          path="/MicroServicesAndCloud"
+          title="MicroServices And Cloud"
+        />
       </div>
-      <div className="hii">
-       Good to have
-       </div>
-       <div className="blocks">
-      <Homecard path="/Spring" title="Spring"/>
-      <Homecard path="/SystemDesign" title="System Design"/>
-      <Homecard path="/BlockChainAndWeb3" title="BlockChain And Web3"/>
-      <Homecard path="/Deployments" title="Deployments"/>
-      <Homecard path="/WebDevCommunity" title="Web Development Community"/>
+      <div className="hii">Good to have</div>
+      <div className="blocks">
+        <Homecard path="/Spring" title="Spring" />
+        <Homecard path="/SystemDesign" title="System Design" />
+        <Homecard path="/BlockChainAndWeb3" title="BlockChain And Web3" />
+        <Homecard path="/Deployments" title="Deployments" />
+        <Homecard path="/WebDevCommunity" title="Web Development Community" />
       </div>
-      
-       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default WebDevlopment
+export default WebDevlopment;
