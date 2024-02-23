438f017e537eb46b3d6e17c952a9c59cb7c63d9a
viplavg <viplavg1999@gmail.com>
2024-02-23 21:44:20 +0530

    Structure KT and reference file data also created

--- src/components/Homecard.jsx
+++ src/components/Homecard.jsx
@@ -1,24 +1,28 @@
-import React,{useState} from 'react'
-import './button.css'
-import { Link } from 'react-router-dom'
-import {useSelector} from "react-redux";
-import { addItem } from '../utils/cartSlice';
-import { useDispatch } from 'react-redux';
-const Homecard = ({path,title}) => {
-  const cartItems=useSelector(store=>store.cart.items);
-  const dispatch=useDispatch();
-  const handleAddItem = () =>{
-    dispatch(addItem("Grapes"));
-  }
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./button.css";
+import { Link } from "react-router-dom";
+const Homecard = ({ path, title }) => {
+  const handleClick = () => {
+    console.log(path, title);
+    toast.success("Added to My Courses.");
+  };
   return (
     <div>
-    <Link to={path}>
-     <button className="a1">{title}
-     {<button className="a2" onClick={()=>handleAddItem()}>Bookmark Course </button>}
-     </button>
+      <ToastContainer />
+      <Link to={path}>
+        <button className="a1">
+          {title}
+          {
+            <button className="a2" onClick={handleClick}>
+              Bookmark Course{" "}
+            </button>
+          }
+        </button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Homecard
+export default Homecard;
