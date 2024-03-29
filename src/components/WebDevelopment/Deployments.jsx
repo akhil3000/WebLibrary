07f1e7ce4bf5886ebc6030495487b36fa4625336
AkhilPolisetty <akhilpolisetty456@gmail.com>
2024-03-29 16:23:42 +0530

    Final Commit before deployment

--- src/components/WebDevelopment/Deployments.jsx
+++ src/components/WebDevelopment/Deployments.jsx
@@ -5,7 +5,7 @@ const Deployments = () => {
   return (
     <div className="menuitem">
        <MenuTitle title="How to host and deploy Html+CSS+Javascript Application" url="https://www.youtube.com/watch?v=Bzwlt3qf1dM"/>
-       <MenuTitle title="How to host and deploy React Application" url="https://www.youtube.com/watch?v=Q9n2mLqXFpU&t=461s"/>
+       <MenuTitle title="How to host and deploy React Application" url="https://www.youtube.com/watch?v=tVzpC5_AC8M&t=147s"/>
        <MenuTitle title="How to host and deploy MERN Application" url="https://www.youtube.com/watch?v=udzURIui5V8&t=293s"/>
     </div>
   )
